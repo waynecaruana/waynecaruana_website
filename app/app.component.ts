88f6c9c6bfd4fbdf35083b2c00bc2c6c511cb751
waynecaruana <caruana91@gmail.com>
2016-04-15 17:25:27 +0200

    updated content

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>Coming Soon!!</h1>'
+    template: '<h1>Coming Soon!!...</h1>'
 })
 export class AppComponent { }
